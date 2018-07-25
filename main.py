533cef01a0aec9d552130138f06deeb1f2c43a4c
Giulio Zhou <giuliozhou8@gmail.com>
2018-07-25 13:20:00 -0400

    Animate subtraction of diagonals

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ class LinearInterpQuad(AnimatedElement):
     def render_obj(self):
         return self.quad
     def update(self, alpha):
-        interm_pts = (1 - alpha) * pts + alpha * end_pts
+        interm_pts = (1 - alpha) * self.pts + alpha * self.end_pts
         self.quad.set_xy(interm_pts)
 
 class Animation():
@@ -89,20 +89,49 @@ num_disp_points = 1e2
 #                    [4, 3]])
 matrix = np.array([[8, -1],
                    [1, 4]])
-vals, vecs = np.linalg.eig(matrix)
-end_vecs = vecs * vals
-xs, ys = [0, 0, 1, 1], [0, 1, 1, 0]
-pts = np.stack([xs, ys], axis=-1)
-end_pts = np.dot(matrix, pts.T).T
-
 fig, ax = plt.subplots()
-eigenvec1 = StaticVector(ax, (0, 0), vecs[:, 0])
-eigenvec2 = StaticVector(ax, (0, 0), vecs[:, 1])
-grid = LinearInterpQuad(ax, xs, ys, end_pts[:, 0], end_pts[:, 1])
-eigenvec1_anim = LinearInterpVector(ax, (0, 0), vecs[:, 0], end_vecs[:, 0])
-eigenvec2_anim = LinearInterpVector(ax, (0, 0), vecs[:, 1], end_vecs[:, 1])
-static_elems = [eigenvec1, eigenvec2]
-animated_elems = [grid, eigenvec1_anim, eigenvec2_anim]
 
+def eigenvec_mult(fig, ax, matrix):
+    vals, vecs = np.linalg.eig(matrix)
+    end_vecs = vecs * vals
+    xs, ys = [0, 0, 1, 1], [0, 1, 1, 0]
+    pts = np.stack([xs, ys], axis=-1)
+    end_pts = np.dot(matrix, pts.T).T
+
+    eigenvec1 = StaticVector(ax, (0, 0), vecs[:, 0])
+    eigenvec2 = StaticVector(ax, (0, 0), vecs[:, 1])
+    grid = LinearInterpQuad(ax, xs, ys, end_pts[:, 0], end_pts[:, 1])
+    eigenvec1_anim = LinearInterpVector(ax, (0, 0), vecs[:, 0], end_vecs[:, 0])
+    eigenvec2_anim = LinearInterpVector(ax, (0, 0), vecs[:, 1], end_vecs[:, 1])
+
+    static_elems = [eigenvec1, eigenvec2]
+    animated_elems = [grid, eigenvec1_anim, eigenvec2_anim]
+    return static_elems, animated_elems
+
+def diagonal_addition_eigenvecs(fig, ax, matrix, rowspace=False):
+    vals, vecs = np.linalg.eig(matrix)
+    xs, ys = [0, 0, 1, 1], [0, 1, 1, 0]
+    pts = np.stack([xs, ys], axis=-1)
+    end_pts = np.dot(matrix, pts.T).T
+    print("Eigenvalues: %s" % str(vals))
+    lower_range = min(0, np.min(vals))
+    upper_range = max(0, np.max(vals))
+    lower_cols = matrix - np.eye(2) * lower_range
+    upper_cols = matrix - np.eye(2) * upper_range
+    if rowspace:
+        lower_cols, upper_cols = lower_cols.T, upper_cols.T
+
+    eigenvec1 = StaticVector(ax, (0, 0), vecs[:, 0])
+    eigenvec2 = StaticVector(ax, (0, 0), vecs[:, 1])
+    grid = LinearInterpQuad(ax, xs, ys, end_pts[:, 0], end_pts[:, 1])
+    column1_anim = LinearInterpVector(ax, (0, 0), lower_cols[:, 0], upper_cols[:, 0])
+    column2_anim = LinearInterpVector(ax, (0, 0), lower_cols[:, 1], upper_cols[:, 1])
+
+    static_elems = [eigenvec1, eigenvec2]
+    animated_elems = [grid, column1_anim, column2_anim]
+    return static_elems, animated_elems
+
+# static_elems, animated_elems = eigenvec_mult(fig, ax, matrix)
+static_elems, animated_elems = diagonal_addition_eigenvecs(fig, ax, matrix)
 anim = Animation(fig, ax, static_elems, animated_elems, num_disp_points)
 anim.animate()
